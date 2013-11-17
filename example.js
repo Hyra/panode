623f22452794c9cfbf6d22777c370b2355737b1a
Hyra <stef@mindthecode.com>
2013-11-17 17:24:35 +0100

    Simplified example code

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
 var pano = require('./panode');
 
 var myPano = pano.generate({
-	lat: '52.347358',
-	lng: '4.847041',
-	outputLocation: require('path').normalize(__dirname + '/_panos/output.jpg')
+	lat: '-37.850493',
+	lng: '144.970284',
+	outputLocation: __dirname + '/output.jpg'
 }, function() {
 	console.log("Panorama saved, do something else");
 });
